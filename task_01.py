163dc9e1d56c51bad08fe788c55694c0c0a57278
Alex Doler <aedoler@gmail.com>
2015-11-12 02:31:31 +0000

    tasks 1 and 2

--- task_01.py
+++ task_01.py
@@ -4,4 +4,21 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """Returns input index of object.
+    Args:
+        var1 (mixed): indexable object
+        var2 (int): index of var1 object to return
+    Returns:
+        input index element, if not input correct index, raises exception
+    Examples:
+        >>>simple_lookup('hello', 2)
+        'l'
+        >>>simple_lookup('hello', 10)
+        The entered index/key does not exist, hello
+        """
+    try:
+        getindex = var1[var2]
+    except LookupError: # or LookupError
+        print 'Warning: Your index/key doesn\'t exist', var1
+        getindex = None
+    return getindex
--- task_02.py
+++ task_02.py
@@ -7,8 +7,30 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """Tests for invalid age"""
     pass
 
+
 def get_age(birthyear):
+    """returns age based on input birthyear.
+    Args:
+        birthyear (int): year of birth
+    Returns:
+        Age of person, unless supposed age is less than 0
+    Examples:
+        >>>get_age(1990)
+        25
+        >>>get_age(2017)
+        Traceback (most recent call last):
+          File "<pyshell#1>", line 1, in <module>
+            get_age(2017)
+          File "/home/vagrant/Desktop/is210-week-12-warmup/task_02.py",
+          line 15, in get_age
+            raise InvalidAgeError('Invalid age')
+        InvalidAgeError: Invalid age
+        """
     age = datetime.datetime.now().year - birthyear
-    return age
+    if age < 0:
+        raise InvalidAgeError('Invalid age')
+    else:
+        return age
